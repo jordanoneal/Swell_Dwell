7a551494a298fe0ab5cd282c8849737e7b44a954
Jordan O'Neal <jordanoneal@Jordans-MBP.localdomain>
2023-11-29 13:45:21 -0700

    Refactored version

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 Swell Dwell is a JavaScript program that integrates Google Maps and the Storm Glass weather data API. The program allows you to explore weather data for any location on the map, or your current location. The weather data is displayed in a table format, with information such as water temperature, wind speed, wave height, and swell period.
 
-## Getting Started
-To use Weather Map, you will need to obtain an API key for the Storm Glass weather data API. You can sign up for a free account at Storm Glass, and get an API key that allows you to make a limited number of requests per day.
-
-Once you have your API key, you can replace the STORM_GLASS_API_KEY constant in the code with your own key. You will also need to include the Google Maps API in your HTML file, by adding a `<script>` tag with your API key.
-
 ## Usage
-To use the program, simply open the index.html file in your browser. You will be prompted to allow the program to access your location. Once you have done so, the map will load and display your current location. You can click on any location on the map to see the weather data for that location.
+
+To use the program, simply open the index.html file in your browser. Once you have done so, you can click on any location on the map to see the weather data for that location.
--- script.js
+++ script.js
@@ -1,109 +1,150 @@
-const STORM_GLASS_API_KEY = '4470429a-793b-11ea-98e7-0242ac130002-4470434e-793b-11ea-98e7-0242ac130002';
+const STORM_GLASS_TEST_KEY = '4470429a-793b-11ea-98e7-0242ac130002-4470434e-793b-11ea-98e7-0242ac130002';
+const GOOGLE_MAPS_TEST_KEY = 'AIzaSyDxwSS__U2ALQTIhAh29c7F9wTjzaKd9vY';
+const MAP_DEFAULT_CENTER = { lat: -4.740360, lng: 55.517000 };
+const MAP_DEFAULT_ZOOM = 10;
 
 let map;
 
+// Load the Google Maps API script with a callback
+function loadGoogleMapsScript() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_TEST_KEY}&callback=initMap`;
+    script.defer = true;
+    script.async = true;
+    document.head.appendChild(script);
+}
+
+// This function will be called when the Google Maps script is loaded
 function initMap() {
-    const myLatLng = { lat: -4.740360, lng: 55.517000 };
+    const MAP_CONTROL_POSITION = google.maps.ControlPosition.TOP_CENTER;
+
     map = new google.maps.Map(document.getElementById("map"), {
-        center: myLatLng,
-        zoom: 10,
+        center: MAP_DEFAULT_CENTER,
+        zoom: MAP_DEFAULT_ZOOM,
     });
 
+    const locationButton = createLocationButton();
+    map.controls[MAP_CONTROL_POSITION].push(locationButton);
+
+    locationButton.addEventListener("click", handleLocationButtonClick);
+
+    map.addListener("click", handleMapClick);
+}
+
+function createLocationButton() {
     const locationButton = document.createElement('button');
     locationButton.textContent = "Pan to Current Location";
     locationButton.classList.add("custom-map-control-button");
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    return locationButton;
+}
 
-    locationButton.addEventListener("click", () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
-                    };
-                    map.setCenter(pos);
-                },
-                () => {
-                    handleLocationError(true, map.getCenter());
-                }
-            );
-        } else {
-            handleLocationError(false, map.getCenter());
-        }
-    });
+function handleLocationButtonClick() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, map.getCenter());
+            }
+        );
+    } else {
+        handleLocationError(false, map.getCenter());
+    }
+}
 
-    map.addListener("click", async (mapsMouseEvent) => {
-        const weatherContainer = document.querySelector('.weather-container');
-        weatherContainer.innerHTML = '';
+async function handleMapClick(mapsMouseEvent) {
+    const weatherContainer = document.querySelector('.weather-container');
+    weatherContainer.innerHTML = ''; //Clear the results of the previous weather data
 
-        const latLng = {
-            lat: mapsMouseEvent.latLng.toJSON().lat,
-            lng: mapsMouseEvent.latLng.toJSON().lng
-        }
+    const latLng = {
+        lat: mapsMouseEvent.latLng.toJSON().lat,
+        lng: mapsMouseEvent.latLng.toJSON().lng
+    };
 
-        const marker = new google.maps.Marker({
-            position: latLng,
-        })
-        marker.setMap(map);
+    const marker = createMarker(latLng);
+    marker.setMap(map);
 
-        const weatherData = await retrieveWeatherData(latLng.lat, latLng.lng);
-        await buildResultsTable(weatherData);
+    const weatherData = await retrieveWeatherData(latLng.lat, latLng.lng);
+    await buildResultsTable(weatherData, marker);
+}
 
-        marker.addListener('click', async () => {
-            const markerLat = marker.getPosition().lat();
-            const markerLng = marker.getPosition().lng();
-
-            const weatherData = await retrieveWeatherData(markerLat, markerLng);
-            await buildResultsTable(weatherData);
-        })
+function createMarker(latLng) {
+    return new google.maps.Marker({
+        position: latLng,
     });
 }
 
 async function retrieveWeatherData(lat, lng) {
     try {
-        const params = ['waterTemperature', 'windSpeed', 'windDirection', 'waveHeight', 'wavePeriod', 'waveDirection', 'swellHeight', 'swellPeriod', 'swellDirection']
+        const params = ['waterTemperature', 'windSpeed', 'windDirection', 'waveHeight', 'wavePeriod', 'waveDirection', 'swellHeight', 'swellPeriod', 'swellDirection'];
         const response = await fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
-            headers: { 'Authorization': STORM_GLASS_API_KEY }
+            headers: { 'Authorization': STORM_GLASS_TEST_KEY }
         });
         const data = await response.json();
-        const first24Hours = data.hours.slice(0, 24);
-
-        return first24Hours;
+        return data.hours.slice(0, 24);
     } catch (err) {
         console.log(err);
         return undefined;
     }
 }
 
-async function buildResultsTable(weatherData) {
-    const weather = document.querySelector('.weather-container');
-    weather.innerHTML = weatherData.map((data) => {
-        const [date, time] = data.time.split('T');
-        const hour = time.toString().substring(0, 5);
+async function buildResultsTable(weatherData, marker) {
+    const weatherContainer = document.querySelector('.weather-container');
+    weatherContainer.innerHTML = weatherData.map(data => createResultsEntry(data)).join('');
 
-        const { waterTemperature, windSpeed, windDirection, waveHeight, wavePeriod, waveDirection, swellHeight, swellPeriod, swellDirection } = data;
+    marker.addListener('click', async () => {
+        const markerLat = marker.getPosition().lat();
+        const markerLng = marker.getPosition().lng();
 
-        return `<div class='results-container'>
-            <ul class='results-list'>
-                <div class='results-header'>
-                    <li>${date}</li>
-                    <li>${hour}</li>                
-                </div>
-                <div class='results-body'>
-                    <li class='results-item'>Water Temp: ${waterTemperature.noaa} °C</li>
-                    <li class='results-item'>Wind Speed: ${windSpeed.noaa} m/s</li>
-                    <li class='results-item'>Wind Direction: ${windDirection.noaa}°</li>
-                    <li class='results-item'>Wave Height: ${waveHeight.noaa}m</li>
-                    <li class='results-item'>Wave Period: ${wavePeriod.noaa}s</li>
-                    <li class='results-item'>Wave Direction: ${waveDirection.noaa}°</li>
-                    <li class='results-item'>Primary Swell Height: ${swellHeight.noaa}m</li>
-                    <li class='results-item'>Primary Swell Period: ${swellPeriod.noaa}s</li>
-                    <li class='results-item'>Primary Swell direction: ${swellDirection.noaa}°</li>
-                </div>
-            </ul>
-        </div>`
-    }).join('');
+        const weatherData = await retrieveWeatherData(markerLat, markerLng);
+        await buildResultsTable(weatherData, marker);
+    });
 }
 
-window.initMap = initMap;
+function createResultsEntry(data) {
+    const [date, time] = data.time.split('T');
+    const hour = time.toString().substring(0, 5);
+    const ampm = getAmPm(hour);
+
+    const { waterTemperature, windSpeed, windDirection, waveHeight, wavePeriod, waveDirection, swellHeight, swellPeriod, swellDirection } = data;
+
+    return `<div class='results-container'>
+        <ul class='results-list'>
+            <div class='results-header'>
+                <li>${date}</li>
+                <li>${convertTo12HourFormat(hour)} ${ampm}</li>                
+            </div>
+            <div class='results-body'>
+                <li class='results-item'>Water Temp: ${waterTemperature.noaa} °C</li>
+                <li class='results-item'>Wind Speed: ${windSpeed.noaa} m/s</li>
+                <li class='results-item'>Wind Direction: ${windDirection.noaa}°</li>
+                <li class='results-item'>Wave Height: ${waveHeight.noaa}m</li>
+                <li class='results-item'>Wave Period: ${wavePeriod.noaa}s</li>
+                <li class='results-item'>Wave Direction: ${waveDirection.noaa}°</li>
+                <li class='results-item'>Primary Swell Height: ${swellHeight.noaa}m</li>
+                <li class='results-item'>Primary Swell Period: ${swellPeriod.noaa}s</li>
+                <li class='results-item'>Primary Swell direction: ${swellDirection.noaa}°</li>
+            </div>
+        </ul>
+    </div>`;
+}
+
+function convertTo12HourFormat(hour) {
+    const [hourPart, minutePart] = hour.split(':');
+    const parsedHour = parseInt(hourPart);
+    const formattedHour = parsedHour % 12 || 12; // Convert 0 to 12
+    return `${formattedHour}:${minutePart}`;
+}
+
+function getAmPm(hour) {
+    const [hourPart] = hour.split(':');
+    const parsedHour = parseInt(hourPart);
+    return parsedHour >= 12 ? 'PM' : 'AM';
+}
+
+loadGoogleMapsScript();
